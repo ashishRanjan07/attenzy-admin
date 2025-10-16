bb89573d44b47611779f2a54d31d3454839165ba
ashishRanjan07 <aviashishranjan@gmail.com>
2025-10-16 17:10:21 +0530

    add

--- src/App.jsx
+++ src/App.jsx
@@ -9,8 +9,8 @@ export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Navigate to="/auth/login" replace />} />
-        <Route path="/auth/login" element={<Login />} />
+        <Route path="/" element={<Navigate to="/login" replace />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/auth/register" element={<Register />} />
         <Route
           path="/auth/forgot-password"
